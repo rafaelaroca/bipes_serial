b2aad55b5afacc7e671ee0b4c5a1cda49cffde04
Jorge Marques <gastmaier@gmail.com>
2021-06-13 20:49:11 -0300

    Adds ESP32 support and tooltip for device selected to `pinout` block.

--- ui/ui.js
+++ ui/ui.js
@@ -14,10 +14,10 @@ function panel (button_, panel_) {
   this.button.onclick = () => {this.showPanel ()};
 }
 panel.prototype.showPanel = function () {
-  let panel_ = BIPES ['responsive'].panels.find ( ({name}) => name === this.panel_);
+  let panel_ = BIPES ['responsive'].panels [this.panel_];
   if(!panel_.show) {
     this.panel.id = 'show';
-    if(panel_.name == '.notify-panel')
+    if(panel_.dom == '.notify-panel')
       BIPES ['notify'].container.id = '';
  } else
     this.panel.id = '';
@@ -51,7 +51,7 @@ notify.prototype.send = function (message) {
   this_message.div.onclick = (ev) => {try {this.panel.removeChild(ev.target.parentNode)}catch(e){};};
   this.panel.appendChild(this_message.div);
 
-  let panel_ = BIPES ['responsive'].panels.find ( ({name}) => name === this.panel_);
+  let panel_ = BIPES ['responsive'].panels [this.panel_];
   if(!panel_.show) {
     this.container.innerHTML = message + "<p>" + this.container.innerHTML;
     this.container.id = 'show';
@@ -69,9 +69,9 @@ notify.prototype.send = function (message) {
 
 function responsive () {
   this.body = get ('body');
-	this.panels = [{name:'.toolbar',x:$em*18.5, y:$em*7.5, show:false},
-	               {name:'.notify-panel',x:$em*18.5, y:0, show:false},
-	               {name:'.language-panel',x:$em*18.5, y:$em*13, show:false}];
+	this.panels = {'.toolbar':{from:'toolbar',x:$em*18.5, y:$em*7.5, show:false},
+	               '.notify-panel':{from:'notify-panel',x:$em*18.5, y:0, show:false},
+	               '.language-panel':{from:'language',x:$em*18.5, y:$em*13, show:false}};
   this.binded = false;
 
   this.body.onclick = (ev) => {this.hidePanels (ev)};
@@ -93,7 +93,8 @@ responsive.prototype.hidePanels = function (ev) {
   if (ev.x !== 0 && ev.y !== 0) {
     let minx = 0;
     let miny = 0;
-    this.panels.forEach ((item) => {
+    for (const prop in this.panels) {
+      let item = this.panels[prop];
       if (item.show === true) {
         if (item.x > minx)
           minx = item.x;
@@ -102,13 +103,13 @@ responsive.prototype.hidePanels = function (ev) {
         if (item.y === 0)
           miny = window.innerHeight;
       }
-    });
-    this.panels.forEach ((item) => {
-      if (((window.innerWidth - minx) > (ev.x) || miny < (ev.y)) && item.show === true) {
-        BIPES [item.name].panel.id='';
-        item.show = false;
+    };
+    for (const prop in this.panels) {
+      if (((window.innerWidth - minx) > (ev.x) || miny < (ev.y)) && this.panels[prop].show === true) {
+        BIPES [this.panels[prop].from].panel.id='';
+        this.panels[prop].show = false;
       }
-    });
+    };
   }
 }
 
@@ -123,47 +124,77 @@ function workspace () {
 
     // setup, could be used and external JSON file instead.
     this.devices = [
-			{name:'ESP8266',
+      {name:'ESP8266',
       title:'<b>ESP8266</b><br>',
-      img:'../../beta2/ui/devinfo/Node-MCU-ESP-12E-Pin-Out-Diagram2.jpg',
-      description:"To use ESP8266, simply connecto to MicroPython board using Wifi. Micropython must be previously installed.",
+      img:'devinfo/Node-MCU-ESP-12E-Pin-Out-Diagram2.jpg',
+      description:"<BR><BR><input type='button' onclick='loadDoc(\"\");' value='Open Documentation' /> <BR><BR>To use ESP8266, simply connecto to MicroPython board using Wifi. Micropython must be previously installed.",
       toolbox:'toolbox_esp8266.xml'},
 
-			{name:'ESP32',
-			title:'<b>ESP32</b><br>',
-			img:'../../beta2/ui/devinfo/ESP32-Pinout.jpg',
-			description:"",
-			toolbox:'toolbox_esp32.xml'},
+      {name:'ESP32',
+      title:'<b>ESP32</b><br>',
+      img:'devinfo/ESP32-Pinout.jpg',
+      description:"",
+      toolbox:'toolbox_esp32.xml'},
 
-			{name:'Nucleo',
-			title:'<b>mBed: NUCLEO-F446RE</b><br>',
-			img:'../../beta2/ui/devinfo/NUCLEO-F446RE-3-500x500.png',
-			description:"",
-			toolbox:'toolbox_stm32.xml'},
+      {name:'wemos_d1_mini',
+      title:'<b>ESP8266 - Wemos D1 Mini</b><br>',
+      img:'devinfo/Wemos-D1-Mini.png',
+      description:"<BR><BR><input type='button' onclick='loadDoc(\"\");' value='Open Documentation' /> <BR><BR>To use ESP8266, simply connecto to MicroPython board using Wifi. Micropython must be previously installed. Figure source: https://devonhubner.org/Using_MicroPython_with_a_WeMos_D1_Mini/",
+      toolbox:'toolbox_esp8266.xml'},
 
-			{name:'BBBlack',
-			title:'<b>Beagle Bone Black</b><br>',
-			img:'../../beta2/ui/devinfo/cape-headers.png',
-			description:"",
-			toolbox:'toolbox_bbblack.xml'},
+      {name:'ESP32-oled',
+      title:'<b>ESP32 board with OLED LCD and Battery</b><br>',
+      img:'devinfo/esp32-oled.png',
+      description:"Yet another ESP32 board",
+      toolbox:'toolbox_esp32.xml'},
 
-			{name:'RPI',
-			title:'',
-			img:'',
-			description:"",
-			toolbox:''},
+      {name:'M5STICK',
+      title:'<b>ESP32</b><br>',
+      img:'devinfo/m5stickc.png',
+      description:"M5STICK-C is based on ESP32 processor. More info at:",
+      toolbox:'toolbox_esp32.xml'},
 
-			{name:'RPI_Pico',
-			title:'<b>Raspberry Pi Pico</b><br>Image: https://microcontrollerslab.com/raspberry-pi-pico-pinout-features-programming-peripherals/<br>',
-			img:'https://microcontrollerslab.com/wp-content/uploads/2021/01/Raspberry-Pi-Pico-pinout-diagram.svg',
-			description:"More info: https://microcontrollerslab.com/raspberry-pi-pico-pinout-features-programming-peripherals/ <br><br>https://www.raspberrypi.org/products/raspberry-pi-pico/",
-			toolbox:'toolbox_rpi_pico.xml'},
+      {name:'Nucleo',
+      title:'<b>mBed: NUCLEO-F446RE</b><br>',
+      img:'devinfo/NUCLEO-F446RE-3-500x500.png',
+      description:"",
+      toolbox:''},
 
-			{name:'UNO',
-			title:'<b>Arduino UNO. Image source: https://content.arduino.cc/assets/Pinout-UNOrev3_latest.png</b><br>',
-			img:'https://content.arduino.cc/assets/Pinout-UNOrev3_latest.png',
-			description:"",
-			toolbox:'toolbox_arduino.xml'}
+      {name:'RPI4',
+      title:'<b>Raspberry Pi 4 </b><br>',
+      img:'devinfo/rpi4.png',
+      description:"Use Raspberry Pi 4 with WebSocketServerREPL. For OpenCV features, please install opencv with apt install python-opencv",
+      toolbox:'toolbox_linux.xml'},
+
+      {name:'EV3',
+      title:'<b>Lego EV3 </b><br>',
+      img:'',
+      description:"Help needed! We need a Lego EV3 to build the blocks to the intelligent brick, or help us designing the blocks, if you have an EV3 Inteligent Brick!",
+      toolbox:''},
+
+      {name:'BBBlack',
+      title:'<b>Beagle Bone Black</b><br>',
+      img:'devinfo/cape-headers.png',
+      description:"",
+      toolbox:'toolbox_bbblack.xml'},
+
+      {name:'BBC',
+      title:'<b>BBC MicroBit</b><br>',
+      img:'devinfo/microbit.png',
+      description:"Image source and more info: <a href=https://microbit.org/get-started/user-guide/features-in-depth/>https://microbit.org/get-started/user-guide/features-in-depth/</a>",
+      toolbox:'toolbox_esp32.xml'},
+
+      {name:'RPI',
+      title:'',
+      img:'',
+      description:"",
+      toolbox:''},
+
+      {name:'UNO',
+      title:'<b>Arduino UNO. Image source: https://content.arduino.cc/assets/Pinout-UNOrev3_latest.png</b><br>',
+      img:'https://content.arduino.cc/assets/Pinout-UNOrev3_latest.png',
+      description:"",
+      toolbox:'toolbox_arduino.xml'},
 
       /* Template
       {name:'',
@@ -181,7 +212,7 @@ function workspace () {
 workspace.prototype.change = function () {
   let selected = this.devices.find ( ({name}) => name === this.selector.value)
   if (selected != undefined) {
-    this.device_title.innerHTML = selected.title,
+    this.device_title.innerHTML = selected.name,
     this.device_img.src = selected.img,
     this.device_desc.innerHTML = selected.description;
 
