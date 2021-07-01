bd91f6f86a7f3a52dffa6cc8d6fc000ca668207e
Jorge Marques <gastmaier@gmail.com>
2021-06-30 23:58:02 -0300

    Creates `workspace` xml tag to stores `DEVICE` and `TIMESTAMP`. Target device changes when the xml file	loads, if no `workspace` is found on the file, nothing happens,	if an invalid device is	set, a notification is sent. Also includes small inprovements to `notify`.

--- ui/code.js
+++ ui/code.js
@@ -253,7 +253,7 @@ Code.renderContent = function() {
     var xmlTextarea = document.getElementById('content_xml');
     var xmlDom = Blockly.Xml.workspaceToDom(Code.workspace);
     var xmlText = Blockly.Xml.domToPrettyText(xmlDom);
-    xmlTextarea.innerText = xmlText;
+    xmlTextarea.innerText = BIPES ['workspace'].writeWorkspace(xmlText);
     Code.attemptCodeGeneration(Blockly.Python, 'py', 'content_python');
   } else if (content.id == 'content_files') {
     var nArea = document.getElementById('content_files');
