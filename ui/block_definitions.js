e3b97279e5ecb1714f40f25b8ce50f7623c458c8
Rafael Vidal Aroca <rafaelaroca@gmail.com>
2021-07-11 07:25:59 -0300

    ADC Fix for ESP32. Thank you Philip!

--- ui/block_definitions.js
+++ ui/block_definitions.js
@@ -456,6 +456,20 @@ Blockly.Blocks['adc'] = {
   }
 };
 
+Blockly.Blocks['esp32_adc'] = {
+  init: function() {
+    this.appendValueInput("pin")
+        .setCheck("Number")
+        .appendField("ESP32 Analog (ADC) Input");
+    this.setOutput(true, null);
+    this.setColour(230);
+ this.setTooltip("Read ESP32 ADC input of specified pin");
+ this.setHelpUrl("http://www.bipes.net.br");
+  }
+};
+
+
+
 
 Blockly.Blocks['adc_pico'] = {
   init: function() {
